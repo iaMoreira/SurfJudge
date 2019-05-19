048988b49e5248b38aef1a19990b0b94325d2833
developer <ianmoreira80@gmail.com>
2019-05-19 18:51:07 -0300

    create screens

---
+++ app/src/main/java/com/devmobil/ian/surfjudge/fragment/Cad1Fragment.java
@@ -0,0 +1,29 @@
+package com.devmobil.ian.surfjudge.fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.devmobil.ian.surfjudge.R;
+
+public class Cad1Fragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_cad1, container, false);
+    }
+
+
+}
